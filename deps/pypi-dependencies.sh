6cd14de98dc6fbf3ea78ab8121b7e101aec62952
UENO, M. <eunosium-1128@live.jp>
2025-12-30 04:44:59 +0000

    Update dependencies with x-checker-data and runtime version to support Python 3.13.9

--- deps/pypi-dependencies.sh
+++ deps/pypi-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Cython version corresponding to the specified numpy version must be manually added to pypi-dependencies.json afterwards
-./flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//48' --requirements-file='requirements.txt' --output=pypi-dependencies --ignore-installed=numpy
+./flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//49' --requirements-file='requirements.txt' --output=pypi-dependencies --checker-data
