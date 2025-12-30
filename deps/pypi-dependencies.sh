ce8110b41bcf04bb2cf6b0c571d495bb21df32e1
UENO, M. <eunosium-1128@live.jp>
2025-12-30 05:27:22 +0000

    Improve pypi-dependencies script formatting and add Cython to requirements

--- deps/pypi-dependencies.sh
+++ deps/pypi-dependencies.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# Cython version corresponding to the specified numpy version must be manually added to pypi-dependencies.json afterwards
-./flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//49' --requirements-file='requirements.txt' --output=pypi-dependencies --checker-data
+./flatpak-builder-tools/pip/flatpak-pip-generator \
+    --runtime='org.gnome.Sdk//49' \
+    --requirements-file='requirements.txt' \
+    --output=pypi-dependencies \
+    --checker-data
